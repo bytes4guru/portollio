bc7ff802920b51b15f3e3cd1d3b980218b643281
Code Astronomer <byte4guru@gmail.com>
2024-12-25 08:23:47 -0700

    buid teste

--- app/api/send/route.js
+++ app/api/send/route.js
@@ -1,10 +1,8 @@
 import { EmailTemplate } from '../../..//components/email-template.jsx';
-import { EmailTemplate2 } from '../../..//components/email-template.jsx';
 import { Resend } from 'resend';
 
-const resend = new Resend(process.env.RESEND_API_KEY);
-
 export async function POST(req) {
+    const resend = new Resend(process.env.RESEND_API_KEY);
     const json = await req.json();
     const { name, email, message } = json;
     try {
@@ -15,14 +13,6 @@ export async function POST(req) {
             subject: `New message from ${name}`,
             react: EmailTemplate({ messageData: message, name: name, email: email }),
         })
-        const resData = await resend.emails.send(
-        {
-            from: `Thomas Feeney <onboarding@resend.dev>`,
-            to: `${email}`,
-            subject: `Reply from Thomas`,
-            react: EmailTemplate2({ name: name}),
-        });
-
         return Response.json(data);
     } catch (error) {
         return Response.json({ error: error.message });
